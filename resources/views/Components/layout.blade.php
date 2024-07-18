06d3f8ae102c40b879a350165e3bf94507d24b9c
BiancaMesa <bianca.koraa@gmail.com>
2024-07-18 22:04:00 +0200

    if helper

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -21,7 +21,7 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <x-nav_link href="/" :active="request()->is('/')">Home</x-nav_link>
               <x-nav_link href="/about" :active="request()->is('about')">About</x-nav_link>
-              <x-nav_link href="/contact" :active="request()->is('contact')">Contact</x-nav_link>
+              <x-nav_link href="/contact" :active="request()->is('contact')" type="button" >Contact</x-nav_link>
             </div>
           </div>
         </div>
