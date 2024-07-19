f5c72f1ac72f8a003f6d866ad9a42b65f12ce62d
BiancaMesa <bianca.koraa@gmail.com>
2024-07-19 12:58:02 +0200

    declaring a variable before the routes so we can use them in routes with use()

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director', 
+        'salary' => '$50,000'
+    ], 
+    [
+        'id' => 2,
+        'title' => 'Programmer', 
+        'salary' => '$10,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher', 
+        'salary' => '$40,000'
+    ]
+];
+
 //We are declaring a route that listens to a get request that is visiting a URL in the browser, in this case, the homepage 
 //When you visit the homepage '/', the run this function. And the function returns a view called 'welcome', welcome is a file.
 //View is a function with argument 'home'.  
@@ -14,50 +32,18 @@ Route::get('/', function () {
 //The first argument is the view jobs and the second argument is the array whose key is jobs and value an array.
 //We can pass a second argument that will be an array and each key will be extracted into a variable once the view is loaded. 
 //When we load our view, we will have access to a jobs variable that is equal to a list of jobs which is an array
-Route::get('/jobs', function () {
+ 
+Route::get('/jobs', function () use($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director', 
-                'salary' => '$50,000'
-            ], 
-            [
-                'id' => 2,
-                'title' => 'Programmer', 
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher', 
-                'salary' => '$40,000'
-            ]
-        ] //variable $jobs. With an array as value and each job has its own array 
-    ]);
+        'jobs' => $jobs]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director', 
-            'salary' => '$50,000'
-        ], 
-        [
-            'id' => 2,
-            'title' => 'Programmer', 
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher', 
-            'salary' => '$40,000'
-        ]
-        ];
+Route::get('/jobs/{id}', function ($id) use($jobs){
+    
 
         //Helper Array, method first. We use this method when we want to find the first item within an array that matches some kind of criteria. 
         //The first argument is the array and the second one is a callback function that will be called for each item within the array. 
